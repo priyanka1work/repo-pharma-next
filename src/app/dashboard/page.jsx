426ad1000df5940ca69da2804b8339873912d8a3
Priyanka <priyanka@Priyankas-MacBook-Air.local>
2026-08-01 08:26:10 +0530

    Adding autheticated logged user

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,21 +1,19 @@
-'use client'
+"use client";
 
 import { isAuthenticated } from "@/lib/auth";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function DashboardPage() {
-    const router=useRouter()
-    useEffect(() => {
-
-        if (!isAuthenticated()) {
-            router.replace("/login");
-        }
-
-    }, [router]);
-    return (
-      
-        <h1 className="text-3xl font-bold">Dashboard</h1>
-  
-    );
+  const router = useRouter();
+  useEffect(() => {
+    if (!isAuthenticated()) {
+      router.replace("/login");
+    }
+  }, [router]);
+  if (!isAuthenticated) {
+    return null;
   }
+
+  return <h1 className="text-3xl font-bold">Dashboard</h1>;
+}
