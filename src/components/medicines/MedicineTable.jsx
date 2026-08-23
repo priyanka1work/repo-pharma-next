6b416f5e4e2c3f9e9b18b8ac77fa7ac7f95d14eb
Priyanka <priyanka@Priyankas-MacBook-Air.local>
2026-08-23 18:48:00 +0530

    Sorting Feature

--- src/components/medicines/MedicineTable.jsx
+++ src/components/medicines/MedicineTable.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import Button from "../ui/Button";
+
 const columns = [
   {
     key: "brandName",
@@ -17,9 +20,38 @@ const columns = [
   },
 ];
 function MedicineTable({ medicines }) {
+  const [sortConfig, setSortConfig] = useState({ key: null, direction: "asc" });
+
+  const handleSort = (key) => {
+    setSortConfig((current) => {
+      if (current.key == key) {
+        return {
+          key,
+          direction: current.direction == "asc" ? "desc" : "asc",
+        };
+      }
+      return {
+        key,
+        direction: "asc",
+      };
+    });
+  };
+  const sortedMedicines = [...medicines].sort((a, b) => {
+    if (!sortConfig.key) {
+  return 0;
+}
+    const aValue = 
+      a[sortConfig.key] ?? "";
+    const bValue = 
+      b[sortConfig.key] ?? "";
+    const camparison=String(aValue).localeCompare(String(bValue))
+    return sortConfig.direction=='asc'? camparison:-camparison
+  });
+
   if (medicines.length === 0) {
     return <p>No medicines found.</p>;
   }
+
   return (
     <div>
       Medicines
@@ -27,17 +59,31 @@ function MedicineTable({ medicines }) {
         <thead>
           <tr>
             {columns.map((item, key) => (
-              <th key={item.key}>{item.label}</th>
+              // <th key={item.key}>{item.label}</th>
+              <th key={item.key} ><Button onClick={() => handleSort(item.key)}>
+                {item.label}
+                {/* Visual Indicator */}
+                {sortConfig.key === item.key
+                  ? sortConfig.direction === "asc"
+                    ? " 🔼"
+                    : " 🔽"
+                  : ""}
+              </Button>
+              
+              </th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {medicines.map((medicine) => (
+          {sortedMedicines.map((medicine) => (
             <tr key={medicine.id} style={{ background: " lightgray" }}>
-              <td>{medicine.brandName}</td>
+              {columns.map((column) => (
+                <td key={column.key}>{medicine[column.key]} </td>
+              ))}
+              {/* <td>{medicine.brandName}</td>
               <td>{medicine.genericName}</td>
               <td>{medicine.manufacturer}</td>
-              <td>{medicine.purpose}</td>
+              <td>{medicine.purpose}</td> */}
             </tr>
           ))}
         </tbody>
